5f238f6412fe9ff3a947708d6c371a0eb2590c55
Stepan Andreev <stepan10andreev@gmail.com>
2023-09-24 11:26:09 +0300

    add i18n rules in eslintrc

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -4,13 +4,13 @@ import { EThemeButton } from './button.interface'
 
 describe('s', () => {
   test('with only first parameter', () => {
-    render(<Button>TEST</Button>)
+    render(<Button>TESs</Button>)
     expect(screen.getByText('TEST')).toBeInTheDocument()
   })
 
   test('with only first parameter', () => {
     render(<Button theme={EThemeButton.GENERAL}>TEST</Button>)
     expect(screen.getByText('TEST')).toHaveClass('general')
-    screen.debug()
+    screen.debug
   })
 })
